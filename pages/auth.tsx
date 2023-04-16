b89af82a829061d05c033db84d4439f98bd14469
Omar-ahm <irycevier@gmail.com>
2023-04-16 10:05:24 +0100

    routed successful login to homepage, moved [...nextauth] to newly created folder within api folder

--- pages/auth.tsx
+++ pages/auth.tsx
@@ -1,9 +1,13 @@
 import axios from "axios";
 import { useCallback, useState } from "react";
 import Input from "@/components/Input";
+import { signIn } from 'next-auth/react';
+import { useRouter } from "next/router";
 
 
 const Auth = () => {
+    const router = useRouter();
+
     const [email, setEmail] = useState('')
     const [name, setName] = useState('');
     const [password, setPassword] = useState('');
@@ -14,6 +18,22 @@ const Auth = () => {
         setVariant((currentVariant) => currentVariant === 'login' ? 'register' : 'login' );
     }, []);
 
+    const login = useCallback(async () => {
+        try {
+            await signIn('credentials' , {
+                email,
+                password,
+                redirect: false,
+                callbackUrl: '/'
+            });
+
+            router.push('/');
+
+        } catch (error) {
+            console.log(error);
+        }
+    }, [email, password])
+
     const register = useCallback(async () => {
         try {
             await axios.post('/api/register', {
@@ -21,11 +41,13 @@ const Auth = () => {
                 name,
                 password
             });
+            login();
         } catch (error) {
             console.log(error)
         }
-    }, [email, name, password]);
+    }, [email, name, password, login]);
 
+    
     return (
         <div className="absolute h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
                 <div className="bg-black w-full h-full lg:bg-opacity-50">
@@ -61,7 +83,7 @@ const Auth = () => {
                                     value={password} 
                                 />
                             </div>
-                            <button onClick={register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+                            <button onClick={variant === 'login' ? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
                                 {variant === 'login' ? 'Login' : 'Sign up'}
                             </button>
                             <p className="text-neutral-500 mt-12">
