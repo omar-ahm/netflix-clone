6879228efe8339fe42e7a19a3be0547ef7a2c642
Omar-ahm <irycevier@gmail.com>
2023-04-17 09:31:04 +0100

    mobile menu items aligned left instead of center

--- components/MobileMenu.tsx
+++ components/MobileMenu.tsx
@@ -10,25 +10,25 @@ const MobileMenu: React.FC<MobileMenuProps> = ({ visible }) => {
     }
 
     return (
-        <div className='bg-black w-56 absolute top-8 left-0 py-5 flex-col border-2 border-gray-800 flex'>
+        <div className='bg-black w-52 absolute top-8 left-0 py-5 flex-col border-2 border-gray-800 flex'>
             <div className='flex flex-col gap-4'>
                 
-                <div className='px-3 text-center text-white hover:underline'>
+                <div className='px-3 text-left text-white hover:underline'>
                     Home
                 </div>
-                <div className='px-3 text-center text-white hover:underline'>
+                <div className='px-3 text-left text-white hover:underline'>
                     Series
                 </div>
-                <div className='px-3 text-center text-white hover:underline'>
+                <div className='px-3 text-left text-white hover:underline'>
                     Films
                 </div>
-                <div className='px-3 text-center text-white hover:underline'>
+                <div className='px-3 text-left text-white hover:underline'>
                     New & Popular
                 </div>
-                <div className='px-3 text-center text-white hover:underline'>
+                <div className='px-3 text-left text-white hover:underline'>
                     My List
                 </div>
-                <div className='px-3 text-center text-white hover:underline'>
+                <div className='px-3 text-left text-white hover:underline'>
                     Browse by languages
                 </div>
 
