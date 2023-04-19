f2b5ede66431a5b9ccaee76226cc21cd08d48a77
Omar-ahm <irycevier@gmail.com>
2023-04-19 06:31:54 +0100

    account name connected and signout icon added

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { signOut } from 'next-auth/react'
+import useCurrentUser from '@/hooks/useCurrentUser';
+import { BiExit } from 'react-icons/bi';
 
 interface AccountMenuProps {
     visible?: boolean;
@@ -8,6 +10,8 @@ interface AccountMenuProps {
 const AccountMenu: React.FC<AccountMenuProps> = ({
     visible
 }) => {
+    const { data } = useCurrentUser();
+
     if (!visible) {
         return null;
     }
@@ -18,12 +22,37 @@ const AccountMenu: React.FC<AccountMenuProps> = ({
             <div className='px-3 group/item flex flex-row gap-3 items-center w-full'>
                 <img className='w-8 rounded-md' src='/images/default-blue.png' alt='' />
                 <p className='text-white text-sm group-hover/item:underline'>
-                    Username
+                    {data?.name}
                 </p>
             </div>
             <hr className='bg-gray-600 border-0 h-px my-4' />
             <div onClick={() => signOut()} className='px-3 text-center text-white text-sm hover:undeline'>
-                Sign out of Netflix
+                
+            <div className='
+                cursor-pointer
+                 px-2 md:px-4
+                 w-auto
+                 h-8
+                 text-s lg:text-md
+                 font-semi-bold
+                 hover:bg-opacity-20
+                 lg:w-auto
+                 lg:h-10
+                 border-2
+                 rounded
+                 flex
+                 justify-center
+                 items-center
+                 transition
+                 hover:bg-neutral-300' >
+                    
+            <BiExit className='
+                    mr-2 ' size={20}/>
+                    Sign out of Netflix
+            </div>
+            
+                
+
             </div> 
 
         </div>
